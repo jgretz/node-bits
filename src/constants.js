9e439ef5a3526d256863c7157d572e61e15b1525
jgretz <jgretz@truefitsolutions.com>
2016-12-27 14:48:22 -0500

    add new datatypes

--- src/constants.js
+++ src/constants.js
@@ -4,3 +4,7 @@ export const DECIMAL = 'DECIMAL';
 export const DOUBLE = 'DOUBLE';
 export const FLOAT = 'FLOAT';
 export const UUID = 'UUID';
+export const STRING = 'STRING';
+export const PASSWORD = 'PASSWORD';
+export const DATE = 'DATE';
+export const BOOLEAN = 'BOOLEAN';

806b95f1b86238cb8aba859cc860c1d03904d806
jgretz <jgretz@truefitsolutions.com>
2017-02-23 10:54:45 -0500

    update readme

--- src/timer.js
+++ src/timer.js
@@ -13,16 +13,22 @@ export const startTimer = (timer = DEFAULT_TIMER, logMethod = log) => {
 
 export const toggleTimer = (timer = DEFAULT_TIMER, logMethod = log) => {
   Timer.get(timer).toggle();
+  const time = Timer.get(timer).time();
 
   if (logMethod) {
-    logMethod(`Timer '${timer}'' toggled at ${Timer.get(timer).time()}.`);
+    logMethod(`Timer '${timer}'' toggled at ${time}.`);
   }
+
+  return time;
 };
 
 export const stopTimer = (timer = DEFAULT_TIMER, logMethod = log) => {
   Timer.get(timer).stop();
+  const time = Timer.get(timer).time();
 
   if (logMethod) {
-    logMethod(`Timer '${timer}'' stopped at ${Timer.get(timer).time()}.`);
+    logMethod(`Timer '${timer}'' stopped at ${time}.`);
   }
+
+  return time;
 };
